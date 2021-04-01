1bdae42b9a231d61b29313f23dce5f477b15258b
delaportePL <pierre-louisdelaporte@hotmail.fr>
2021-04-02 00:03:03 +0200

    fresh start !

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # PPE3
+
+Composants installés : 
+
+composer require orm 
+# ORM 
+
+composer require symfony/maker-bundle --dev
+# Maker (entity etc)
+
+composer req debug
+# Outils de débuggage
+
+composer req twig
+
+composer req profiler --dev 
+# Permet d'obtenir des informations précises sur les requêtes
+# WARNING installer uniquement en mode dev (--dev), ne surtout pas l'installer en mode production, au risque de mener le projet à des problèmes de sécurité majeurs
+
+composer req asset
+# Permet de donner le chemin des assets avec une fonction, donner une url d'image en dur n'est plus d'actualité (problème de versionning etc.)
+
+composer req security form validator
+# Pour les formulaires, connexion etc (3 composants)
+
+
+
+afficher les composants installés
+# composer show
+
+
+Étapes :
+
+symfony console doctrine:database:create
+
---
+++ src/Entity/Medicament.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MedicamentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MedicamentRepository::class)
+ */
+class Medicament
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom_commercial;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $famille;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $composition;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $effets;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $contre_indications;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $prix_echantillon;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNomCommercial(): ?string
+    {
+        return $this->nom_commercial;
+    }
+
+    public function setNomCommercial(string $nom_commercial): self
+    {
+        $this->nom_commercial = $nom_commercial;
+
+        return $this;
+    }
+
+    public function getFamille(): ?string
+    {
+        return $this->famille;
+    }
+
+    public function setFamille(string $famille): self
+    {
+        $this->famille = $famille;
+
+        return $this;
+    }
+
+    public function getComposition(): ?string
+    {
+        return $this->composition;
+    }
+
+    public function setComposition(string $composition): self
+    {
+        $this->composition = $composition;
+
+        return $this;
+    }
+
+    public function getEffets(): ?string
+    {
+        return $this->effets;
+    }
+
+    public function setEffets(string $effets): self
+    {
+        $this->effets = $effets;
+
+        return $this;
+    }
+
+    public function getContreIndications(): ?string
+    {
+        return $this->contre_indications;
+    }
+
+    public function setContreIndications(string $contre_indications): self
+    {
+        $this->contre_indications = $contre_indications;
+
+        return $this;
+    }
+
+    public function getPrixEchantillon(): ?int
+    {
+        return $this->prix_echantillon;
+    }
+
+    public function setPrixEchantillon(int $prix_echantillon): self
+    {
+        $this->prix_echantillon = $prix_echantillon;
+
+        return $this;
+    }
+}
